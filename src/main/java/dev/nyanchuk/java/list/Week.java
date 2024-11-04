bbdd0d2c704ce68cfc4d17a87986051ad880f9b8
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-11-04 23:41:13 +0100

    ci: add initializeWeek

--- src/main/java/dev/nyanchuk/java/list/Week.java
+++ src/main/java/dev/nyanchuk/java/list/Week.java
@@ -11,5 +11,13 @@ public class Week {
     // Constructor
     public Week() {
         this.days = new ArrayList<>();
+        initializeWeek();
+    }
+
+    // Initialize the week with all days
+    private void initializeWeek() {
+        for (Day day : Day.values()) {
+            days.add(day);
+        }
     }
 }
