fb54ad05bb667a8e5f608b029712988ac7eb1458
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-11-04 23:40:05 +0100

    feat: add init and constructor en la class Week

--- src/main/java/dev/nyanchuk/java/list/Week.java
+++ src/main/java/dev/nyanchuk/java/list/Week.java
@@ -1,5 +1,15 @@
 package dev.nyanchuk.java.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Week {
 
+    // Initialization
+    private List<Day> days;
+
+    // Constructor
+    public Week() {
+        this.days = new ArrayList<>();
+    }
 }
